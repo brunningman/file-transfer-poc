9f8a83369cfbe5ae464c51453a0c64b5610e0fe9
brunningman <abrunner221@gmail.com>
2019-12-29 13:21:28 -0800

    added morgan logger middleware

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const morgan = require('morgan');
 
+app.use(morgan('short'));
 app.use(express.static(path.resolve('public')));
 
 app.listen(PORT, () => {
